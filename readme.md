6edab43603e021e996c70d481b5bafadcf8acb29
YoshiRi <yoshiyoshidetteiu@gmail.com>
2020-08-30 03:17:46 +0900

    update using subprocess

--- readme.md
+++ readme.md
@@ -1,2 +1,23 @@
 # CPU temperature visualizar for python
 
+## os popen 
+
+```
+fh = os.popen(cmd)
+output = fh.read()
+print(output)
+fh.close()
+```
+
+## Subprocess popen multiple line
+
+- separate cmd with & will work
+- not work for some program
+
+Tips: we need to escape command!
+
+```
+cmd = """wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature /value"""
+```
+
+- remove \n and \r
--- win10cputemp.py
+++ win10cputemp.py
@@ -6,16 +6,26 @@ I am not sure, if the temperature reading is right.
 
 import os
 import time
+import subprocess
+import re
 
+# this code some how has error
+#cmd = """for /f "skip=1 tokens=2 delims==" %%A in ('wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature /value') do set /a "HunDegCel=(%%~A*10)-27315" & echo %HunDegCel:~0,-2%.%HunDegCel:~-2% """
+
+cmd = """wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature /value"""
 
 
 # while loop
 while True:
     try:
-        fh = os.popen("get_cpu_temp.bat")
-        output = fh.read()
-        print(output[:5])
-        fh.close()
+        ch = subprocess.check_output(cmd, shell=True)
+        out = ch.decode("SHIFT-JIS")
+        # getoutput
+        tmp = [x for x in re.split('[\n\r=]', out) if x if x.isdigit()][0]
+        #print([x for x in re.split('[\n\r=]', out) if x if x.isdigit()][0])
+        c_tmp =int(tmp)/10.0 - 273.15
+        print(c_tmp)
         time.sleep(1)
     except KeyboardInterrupt:
         exit(0)
+
